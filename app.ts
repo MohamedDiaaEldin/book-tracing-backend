e6decc514395cf01b74ab8c6470f290d60af574a
MohamedDiaaEldin <mdiaan404@gmail.com>
2024-05-28 19:15:18 +0300

    Feat : Add Get '/books' EndPoint - Retrieves a token - Returns the first ten books assigned with shelves

--- app.ts
+++ app.ts
@@ -1,14 +1,33 @@
 import express, { Request, Response } from 'express';
 import dotenv from 'dotenv';
-
 dotenv.config();
+// import {getAll} from './controllers/BookController'
+import {getAll} from './controllers/BookController';
+
 
 const app = express();
 
 app.use(express.json());
 
+
+
+
 app.get('/', (req: Request, res: Response): void => {
   res.send('Welcome to the home page!');
 });
 
-export default app;
+
+
+
+// TODO: Add middleware to check for Authorization token
+// If not found in the database, create a new user with token
+// Get the first ten books
+// Assign each group of books with a shelf (currentlyReading, wantToRead, read)
+app.get('/books', getAll);
+
+
+
+
+
+
+export default app
