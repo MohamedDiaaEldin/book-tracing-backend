bbbee47957513530499b139e809a038af23fc60a
MohamedDiaaEldin <mdiaan404@gmail.com>
2024-05-28 15:33:07 +0300

    Fix : Update Book id Data Type from Integer into String

--- migrations/20240528122427-books-table.js
+++ migrations/20240528122427-books-table.js
@@ -7,7 +7,7 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.createTable('books', {
       id: {
-        type: Sequelize.INTEGER,
+        type: Sequelize.STRING,
         primaryKey: true,
       },
       title: {
