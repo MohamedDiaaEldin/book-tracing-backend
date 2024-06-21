6f568cbb5f15c2aa414f1d115ad4890561179f70
MohamedDiaaEldin <mdiaan404@gmail.com>
2024-06-21 21:23:02 +0300

    refactor : update sequelize configuration - use url to connect to database instead of credentials

--- src/sequelize.ts
+++ src/sequelize.ts
@@ -10,17 +10,12 @@ dotenv.config(  );
 
 const dialect: Dialect = process.env.DIALECT as Dialect || 'postgres';
 
-console.log('At Sequelize');
-// Initialize Sequelize with database connection options
-const sequelize = new Sequelize({
-  database: process.env.DB_NAME,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  dialect: dialect,
-  models:[Book, User, UserBook],
-  logging:false
 
+// Initialize Sequelize with database connection options using URL
+const sequelize = new Sequelize(process.env.DATABASE_URL!, {
+  dialect: dialect,
+  models: [Book, User, UserBook],
+  logging: false
 });
 
 // Export Sequelize instance for use in other parts of the application
