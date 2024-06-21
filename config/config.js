b06929d718d92baf5c8468c013e7e975069d4dd3
MohamedDiaaEldin <mdiaan404@gmail.com>
2024-06-21 22:32:14 +0300

    feat : add cors for all domains

--- config/config.js
+++ config/config.js
@@ -4,11 +4,11 @@ require('dotenv').config();
 
 const config = {
   development: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
+    use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
+    dialectOptions: {
+      ssl: true
+    }
   },
   test: {
     username: 'root',
