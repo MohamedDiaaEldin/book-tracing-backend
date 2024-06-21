6e9563a580c507a6608eb5f0d13d5e0a51c0c0e5
MohamedDiaaEldin <mdiaan404@gmail.com>
2024-06-21 21:04:25 +0300

    refactor : add inline docs for searchValidator - refactor validator

--- src/middlewares/searchValidator.ts
+++ src/middlewares/searchValidator.ts
@@ -2,9 +2,14 @@ import { Request, Response, NextFunction } from "express";
 
 
 
+
+/**
+ * @description Validate the search query and maxResults that are passed in the body of the request
+ * @returns 400 if the query or maxResults is not provided
+ */
 const searchValidator = (req: Request, res: Response, next: NextFunction) => {
-  const { word, maxResults } = req.body;
-  if (!word || !maxResults) {
+  const { query, maxResults } = req.body;
+  if (!query || !maxResults) {
     return res.status(400).json({ message: "Word and maxResults are required" });
   }
   next();
