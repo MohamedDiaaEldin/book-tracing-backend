6009bdd6030a7eb5bf3cd410c8026ea47719dbb6
MohamedDiaaEldin <mdiaan404@gmail.com>
2024-06-21 20:45:38 +0300

    Feat : Configure Search validator with search route
    docs : Add inline docs for the routes

--- app.ts
+++ app.ts
@@ -1,30 +1,57 @@
 import express, { Request, Response } from 'express';
-import dotenv from 'dotenv';
 import cors from 'cors';
-dotenv.config();
-// import {getAll} from './controllers/BookController'
+import searchValidator from './src/middlewares/searchValidator';
 import {getAll} from './src/controllers/BookController';
 import { authenticate } from './src/middlewares/authentication';
 import { search } from './src/controllers/BookController';
 import { updateShelf } from './src/controllers/BookController';
 
+
 const app = express();
 
+// use middleware to parse the body of the request
 app.use(express.json());
 app.use(cors());// enable cors for all routes and domains
 
+// define the home route - health check
 app.get('/', (req: Request, res: Response): void => {
   res.send('Welcome to the home page!');
 });
 
-// TODO: Add middleware to check for Authorization token
-// If not found in the database, create a new user with token
-// Get the first ten books
-// Assign each group of books with a shelf (currentlyReading, wantToRead, read)
+
+
+
+/**
+ * Get all books from the database
+ * @route GET /books
+ * @description Get all books from the database if user is signed in else return 10 default books books (defined in authenticate middleware)
+ * @returns - the list of books
+ */
 app.get('/books',authenticate,  getAll);
 
-app.post('/search', search);
 
+
+/**
+ * Search for books
+ * @route POST /search
+ * @description Search for books
+ * @param word - the word to search for
+ * @param maxResults - the maximum number of results to return
+ * @returns - the list of books
+ */
+app.post('/search', searchValidator, search);
+
+
+/**
+ * Update the shelf of a book
+ * @route PUT /books/:bookId
+ * @description Update the shelf of a book
+ * @param bookId - the id of the book
+ * @param shelf - the shelf to update the book to
+ * @returns success message
+ */
 app.put('/books/:bookId',authenticate, updateShelf);
+
+
 
 export default app;
