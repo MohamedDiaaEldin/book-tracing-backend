ce88abbee2198ecd558686a4413f34cff9798062
MohamedDiaaEldin <mdiaan404@gmail.com>
2024-06-22 14:49:42 +0300

    Refactor : logging request headers

--- src/middlewares/logging.ts
+++ src/middlewares/logging.ts
@@ -3,10 +3,7 @@
 import { Request, Response, NextFunction } from 'express'
 
 export const loggingRequest = (req:Request, res:Response, next:NextFunction)=>{
-    const body = req.body
-    const contentType = req.headers['content-type']
-    const userAgent = req.headers['user-agent']
-    console.log('LOGGING.. ', '- Request Body: ', body, ' - Request Content Type: ', contentType, ' - Request User Agent: ', userAgent)
-    console.log('request', req)
+    
+    console.log('request headers ', req.headers)
     next()
 }
