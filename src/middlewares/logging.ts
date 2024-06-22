16ba540f1b62d7e90f4a47146de45876565bae4b
MohamedDiaaEldin <mdiaan404@gmail.com>
2024-06-22 14:35:35 +0300

    Feat : Add logging middleware for request data

--- src/middlewares/logging.ts
+++ src/middlewares/logging.ts
@@ -7,5 +7,6 @@ export const loggingRequest = (req:Request, res:Response, next:NextFunction)=>{
     const contentType = req.headers['content-type']
     const userAgent = req.headers['user-agent']
     console.log('LOGGING.. ', '- Request Body: ', body, ' - Request Content Type: ', contentType, ' - Request User Agent: ', userAgent)
+    console.log('request', req)
     next()
 }
