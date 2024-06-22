80a9b7f89963c2cd678e1234a23cfebff0fc2619
MohamedDiaaEldin <mdiaan404@gmail.com>
2024-06-22 19:28:43 +0300

    Feat : Update Pool Configuration - Increase max connection to 1000 - Increase acquire connection limit to 3000 ms - Increase Idle to 10000 ms

--- src/sequelize.ts
+++ src/sequelize.ts
@@ -10,10 +10,22 @@ dotenv.config(  );
 
 const dialect: Dialect = process.env.DIALECT as Dialect || 'postgres';
 
-let options = {};
 
+// self signed certificate
+let options = {
+  pool: {
+    max: 1000,
+    min: 0,
+    acquire: 30000, // 30 seconds
+    idle: 10000, // 10 seconds
+  },
+};
+
+
+const DATABASE_URL = process.env.DATABASE_URL;
+console.log(DATABASE_URL);
 // Initialize Sequelize with database connection options using URL
-const sequelize = new Sequelize(process.env.DATABASE_URL!, {
+const sequelize = new Sequelize(DATABASE_URL!, {
   ...options,
   dialect: dialect,
   models: [Book, User, UserBook],
